05d68b0b8b6fe4753e738ddfc793878ce56362a4
すかさき <tech-camp@tech-camp-1-054-no-MacBook-Air.local>
2017-03-01 13:01:31 +0900

    ログイン前とログイン後コンテンツを分割

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,6 +1,11 @@
 class PlansController < ApplicationController
 
    def index
+    redirect_to action: :index_nologin unless user_signed_in?
    end
-   
+
+   def index_nologin
+
+   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
    get 'index' => 'plans#index'
+   get 'index_nologin' => 'plans#index_nologin'
    root 'plans#index'
 end
